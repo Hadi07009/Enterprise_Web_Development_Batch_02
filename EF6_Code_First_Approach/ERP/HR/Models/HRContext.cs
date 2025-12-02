981f69b40f39c712b49290c9a15c5cc5746c733a
Hadi07009 <hadi.mbstu@gmail.com>
2025-12-02 12:34:59 +0600

    EF6 + ASP.NET MVC 5 — Code-First Approach​

---
+++ EF6_Code_First_Approach/ERP/HR/Models/HRContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace HR.Models
+{
+    public class HRContext:DbContext
+    {
+        public HRContext():base("dberpbatch2connection")
+        {
+        }
+
+        
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
