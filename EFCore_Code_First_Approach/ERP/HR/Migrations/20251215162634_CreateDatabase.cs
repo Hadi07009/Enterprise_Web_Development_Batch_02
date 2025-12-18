64f366ec1b09f2927b413363c12e425fc8ed6788
Hadi07009 <hadi.mbstu@gmail.com>
2025-12-18 12:13:42 +0600

    EFCore + ASP.NET Core — Code-First Approach​

---
+++ EFCore_Code_First_Approach/ERP/HR/Migrations/20251215162634_CreateDatabase.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HR.Migrations
+{
+    public partial class CreateDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "employees",
+                columns: table => new
+                {
+                    EmployeeID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    EmployeeName = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    MobileNo = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_employees", x => x.EmployeeID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "employees");
+        }
+    }
+}
